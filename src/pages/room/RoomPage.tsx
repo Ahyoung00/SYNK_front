faf8fb064be6e0161db7feeda6d88a4bd2ae76c2
이아영 <ahyeong@iayeong-ui-MacBookAir.local>
2026-07-02 09:47:50 +0900

    design: 채팅 버튼 텍스트 제거 및 아이콘 크기 확대 (Closes #212)

--- src/pages/room/RoomPage.tsx
+++ src/pages/room/RoomPage.tsx
@@ -180,10 +180,9 @@ export default function RoomPage() {
             onClick={() => navigate(ROUTES.ROOM_CHAT(id))}
             aria-label="채팅"
           >
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+            <svg width="22" height="22" viewBox="0 0 24 24" fill="none" aria-hidden="true">
               <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" fill="#fff" />
             </svg>
-            채팅
           </button>
         }
       />
